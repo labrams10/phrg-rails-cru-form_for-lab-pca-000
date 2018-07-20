f451826e982e38795c1fd0b59d0cb52b37913ce2
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-07-19 23:48:09 -0400

    finished cru form_for lab

---
+++ db/migrate/20180720025634_create_genres.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateGenres < ActiveRecord::Migration
+  def change
+    create_table :genres do |t|
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20180720025919_create_artists.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.text :bio
+
+      t.timestamps null: false
+    end
+  end
+end
